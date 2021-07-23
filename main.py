71f53199ee5d7b9e291ea92894ae249216035069
Renato Lacerda <renato.lacerda@in8.com.br>
2021-07-23 17:03:26 -0300

    feat: Create Routes to Get Notebooks

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask, json, jsonify
+
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+with open('dados.json', 'r') as myfile:
+    data=myfile.read()
+
+objectsNotebooks = json.loads(data)
+
+@app.route('/getall',methods=['GET'])
+def getNotebooks():
+    return jsonify(objectsNotebooks)
+@app.route('/todo/create',methods=['POST'])
+def createNotebooks():
+    return 'Create new notebook'
